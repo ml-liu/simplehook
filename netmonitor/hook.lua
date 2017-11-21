f97196c64227d52e39a780ae666db796cc8f7163
ml-liu <275913647@qq.com>
2017-11-21 16:54:20 +0800

    修改网络监控

--- netmonitor/hook.lua
+++ netmonitor/hook.lua
@@ -1,35 +1,12 @@
 
-local g_f = nil
 
 function writelog(str)
-   if(g_f == nil and get_tid)
-   then
-      g_f = io.open(string.format("/tmp/simplehook_thread_%x",get_tid()),'w')
-   end
-
-   print(str)
-
-   if g_f
-   then 
-       g_f:write(str)
-       g_f:write("\n")
-   end
+   log_out(str)
 end
 
 
 writelog("hook init");
-
-
-local frame_idx = 0;
-
-
-
-
-
-function frame_notify()
-   profiler_frame(frame_idx, 0);
-   frame_idx = frame_idx + 1;
-end
+ 
    
 
 function listen(a,b)
@@ -41,12 +18,20 @@ end
 
 
 function recv(fd,buf,len,flags,ret,elaps)
-   writelog(string.format("recv fd %d len %d flags %d ret %d elaps %d us", fd, len, flags, ret, elaps))
+   local l_ip,l_port = get_sock_info(fd)
+   local ip,port = get_peer_info(fd)
+   local isnonblock = is_sock_nonblock(fd)
+
+   writelog(string.format("recv fd %d len %d flags %d ret %d elaps %d us (%s:%d %s:%d) isnonblock %d", fd, len, flags, ret, elaps, l_ip,l_port,ip,port, isnonblock))
 end
 
 
 function send(fd, buf, len, flags, ret, elaps)
-   writelog(string.format("send fd %d len %d flags %d, ret %d elaps %d us", fd, len, flags, ret, eplaps))
+   local l_ip,l_port = get_sock_info(fd)
+   local ip,port = get_peer_info(fd)
+   local isnonblock = is_sock_nonblock(fd)
+
+   writelog(string.format("send fd %d len %d flags %d, ret %d elaps %d us  (%s:%d %s:%d) isnonblock %d", fd, len, flags, ret, eplaps, l_ip,l_port,ip,port, isnonblock))
 
 end
 
@@ -58,7 +43,9 @@ end
 
 
 function accept(fd, d1, d2)
-  writelog(string.format("accept fd %d", fd))
+  local l_ip,l_port = get_sock_info(fd)
+  local isnonblock = is_sock_nonblock(fd)
+  writelog(string.format("accept fd %d (%s:%d) isnonblock %d", fd, l_ip,l_port,isnonblock))
 end
 
 
@@ -67,33 +54,17 @@ function socket(d,t,p,ret,elaps)
 end
 
 
-function close(td)
+function close(fd)
   writelog(string.format("close fd %d", fd))
 end
 
 
 
-if(start_luaprofiler)
-then
-  --local a = get_so_load_base("/home/jx3/server/bin64/libEngine_Lua5X64.so")
-  --start_luaprofiler(0x49330 + a)
-end
-
-
-writelog("sdfsdfsdfsfd")
-
 if(set_hook)
 then
---   set_hook(0x5290b0,"vP","frame_notify")
    
       
-  writelog("yeap");
   local l=get_fun_addr("listen")
-  writelog("yeap");
-
-  
-  writelog(string.format("listen:%x",l))
-  writelog("yeap");
   
   set_hook(l, "iii", "listen")
  
