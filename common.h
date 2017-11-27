962d8307c74d0cbc79acf7f5facbc056aa098553
ml-liu <275913647@qq.com>
2017-11-27 17:08:03 +0800

    接口导出

--- common.h
+++ common.h
@@ -11,6 +11,8 @@ __attribute((visibility("default"))) double current_tick();
 void log_init();
 __attribute((visibility("default")))  void ffi_log_out(char* str);
 unsigned long long current_msecond();
+__attribute((visibility("default")))  char* ffi_get_sock_info(int fd);
+__attribute((visibility("default")))  char* ffi_get_peer_info(int fd);
 #ifdef __cplusplus
 }
 #endif
