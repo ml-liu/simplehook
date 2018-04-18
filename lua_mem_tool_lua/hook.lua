56c4de47990ab2cb280c2e527483c2d937a8608c
ml-liu <275913647@qq.com>
2018-04-18 14:36:57 +0800

    修改hook.lua

--- lua_mem_tool_lua/hook.lua
+++ lua_mem_tool_lua/hook.lua
@@ -1,6 +1,9 @@
 print("hook init 1111");
 
 
+local soaddr  = 0 ---get_so_load_base("xxx.so")
+
+
 
 local lua_newstate = 0x19300a0
 local lua_getstack = 0x0
@@ -8,6 +11,6 @@ local lua_getinfo = 0x0
 local lua_sethook = 0x0
 if(hook_luajit_mem)      
 then
-   hook_luajit_mem(lua_newstate,lua_getstack,lua_getinfo, lua_sethook)
+   hook_luajit_mem(lua_newstate + soaddr,lua_getstack + soaddr,lua_getinfo + soaddr, lua_sethook + soaddr)
 end
 
